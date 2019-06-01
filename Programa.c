24991736a804a587d13ada6ae2c919a996b5cda2
edabruzzo <emmanuel.oliveira3@gmail.com>
2019-06-01 13:13:02 -0300

    Ignora arquivos *.exe

--- Programa.c
+++ Programa.c
@@ -3,7 +3,17 @@ REFERÊNCIAS:
 
 https://www.ime.usp.br/~pf/mac0122-2002/aulas/trees.html
 https://pt.stackoverflow.com/questions/311999/estrutura-de-dados-express%C3%B5es-aritm%C3%A9ticas
+https://slideplayer.com.br/slide/2694123/
 
+https://www.ime.usp.br/~pf/mac0122-2002/aulas/stacks.html
+https://www.cin.ufpe.br/~garme/public/(ebook)Estruturas%20de%20Dados%20Usando%20C%20(Tenenbaum).pdf
+http://www.maxpezzin.com.br/aulas/2_ESW_Estruturas_de_Dados/Pesquisa/apostila_estruturas_dados.pdf
+https://www.ime.usp.br/~pjssilva/disciplinas/2009/mac122/codigo/pilha/
+https://www.tutorialspoint.com/cprogramming/c_memory_management.htm
+https://www.ic.unicamp.br/~lehilton/mc202de/09-unidade-pilhaaplicacao.pdf
+https://www.ic.unicamp.br/~lehilton/mc202de/
+https://document.onl/documents/estrutura-de-dados-i-campus-de-santo-a-oberarquivosdisciplinasestruturaiisiapostila.html
+http://www.ic.uff.br/~ferraz/EDI/Ppt/P05ExpressoesAritmeticas1.ppt
 
 
 */
@@ -44,27 +54,8 @@ char expressaoPOSFIXA[] = {'a', 'b', 'c', '*', 'd', 'e', '+', '*', '+', 'f', '*'
 int main(){
 	
 	converterExpressaoAritmetica(expressaoPREFIXA);	
-	
-	/*
-	
-	Arvore *arvoreINFIXA = montaArvoreBinaria(expressaoINFIXA);
-	imprimeOrdemPrefixa(arvoreINFIXA);
-	printf("\n");
-	imprimeOrdemInfixa(arvoreINFIXA);
-	printf("\n");
-	imprimeOrdemPosFixa(arvoreINFIXA);
-	printf("\n");
-	
-
-	Arvore *arvorePOSFIXA = montaArvoreBinaria(expressaoPOSFIXA);
-	imprimeOrdemPrefixa(arvorePOSFIXA);
-	printf("\n");
-	imprimeOrdemInfixa(arvorePOSFIXA);
-	printf("\n");
-	imprimeOrdemPosFixa(arvorePOSFIXA);
-	printf("\n");
-
-*/
+	converterExpressaoAritmetica(expressaoINFIXA);	
+	converterExpressaoAritmetica(expressaoPOSFIXA);	
 	
 	
 }
@@ -115,9 +106,9 @@ int ehOperador(char c){
 
 void atribuirValorExpressaoGlobal(char expressaoAvaliada[]){
 	
-	int i;
+	int i = 0;
 	printf("Expressao avaliada :   ");
-	for(i=0; i<strlen(expressaoAvaliada); i++){
+	for(i; i<strlen(expressaoAvaliada); i++){
 		printf("%c", expressaoAvaliada[i]);
 	 	expressao[i] = expressaoAvaliada[i];
     	
